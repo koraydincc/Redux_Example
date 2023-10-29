4d0a9ed6743ab29f1c260313a7975280de1d02a3
= <koraydincc@icloud.com>
2023-10-29 22:31:43 +0300

    Example
    Example

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav>
+        <div>
+            <h3>Teknoloji Uygulaması</h3>
+            <div>
+                <p>0</p>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
